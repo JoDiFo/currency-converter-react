83a4be24575badf1b42c26d9f4ace38a58ad9e7c
alexfokin <sasafokin2@gmail.com>
2024-02-20 09:13:14 +0300

    Initial commit

---
+++ server/Program.cs
@@ -0,0 +1,30 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => "Hello from server");
+
+app.Run();
+
+
