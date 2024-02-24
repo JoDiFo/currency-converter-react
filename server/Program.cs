123040cfd2516374258c354545a006b1a271e793
alexfokin <sasafokin2@gmail.com>
2024-02-24 18:05:28 +0300

    merge server with master

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Microsoft.VisualBasic;
+
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +27,51 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapGet("/", () => "Hello from server");
+
+app.MapGet("/api/codes", () =>
+{
+    string codesUrl = url + "/codes";
+    try
+    {
+        var codesResponse = new WebClient().DownloadString(codesUrl);
+        return codesResponse;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        return "fail";
+    }
+});
+
+app.MapGet("/api/convert/from={from}&to={to}&amount={amount}", (string from, string to, double amount) =>
+{
+    string convertString = $"{url}/pair/{from}/{to}/{amount}";
+    try
+    {
+        var convertResponse = new WebClient().DownloadString(convertString);
+        return convertResponse;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        return "fail";
+    }
+});
+
+app.MapGet("/api/latest/code={code}", (string code) =>
+{
+    string latestUrl = $"{url}/latest/{code}";
+    try
+    {
+        var latestResponse = new WebClient().DownloadString(latestUrl);
+        return latestResponse;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        return "fail";
+    }
+});
 
 app.Run();
 
